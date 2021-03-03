e441eb7534831a1a29eede04210064049cf44c6a
“tim <saxe@quicklogic.com>
2021-03-02 17:00:26 -0800

    updates for auto-test
    Signed-off-by: “tim <saxe@quicklogic.com>

--- uart_test/blinky/main.c
+++ uart_test/blinky/main.c
@@ -52,6 +52,7 @@
 #include "hal/include/hal_fll.h"
 #include "hal/include/hal_irq.h"
 #include "hal/include/hal_gpio.h"
+#include "perpih-tasks/include/write_uart_task.h"
 
 /******************************************************************************
  * This project provides two demo applications.  A simple blinky style project,
